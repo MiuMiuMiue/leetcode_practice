3e2aa292473395c89ebdd6984c7ebfc732f7c111
MiuMiuMiue <zhey16@uci.edu>
2024-08-30 15:47:37 +0800

    revise

--- majorityElement.py
+++ majorityElement.py
@@ -2,4 +2,13 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        pass
+        check = {}
+        for num in nums:
+            if num not in check:
+                check[num] = 1
+            else:
+                check[num] += 1
+        
+        for num in check:
+            if check[num] > len(nums) // 2:
+                return num
