305939313ed564387ff3c7e8632d20da8fb25367
MiuMiuMiue <yuanzhe2001@gmail.com>
2024-10-04 16:34:06 -0700

    2 mediums

---
+++ medium/bestTimeToBuyAndSellStock2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        i = 0
+        localMin = 0
+        totalProfit = 0
+
+        while i < len(prices) - 1:
+            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
+                i += 1
+            localMin = prices[i]
+            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
+                i += 1
+            totalProfit += prices[i] - localMin
+        
+        return totalProfit
